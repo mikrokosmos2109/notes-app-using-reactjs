11ff55e76e36fdae130c66b8eb1a51f5d3eb4ecc
mikrokosmos2109 <keertilata20@gmail.com>
2022-07-10 18:52:47 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className='header'>
+			<h1>Sticky Notes 📝</h1>
+			{/* <button
+				onClick={() =>
+					handleToggleDarkMode(
+						(previousDarkMode) => !previousDarkMode
+					)
+				}
+				className='save'
+			>
+				Switch Mode
+			</button> */}
+		</div>
+	);
+};
+
+export default Header;
